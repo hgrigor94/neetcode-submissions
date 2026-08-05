003e6632f874f91f1509223354ee86ac40ff0cac
hgrigor94 <60913448+hgrigor94@users.noreply.github.com>
2026-08-05 11:33:42 +0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution
+{
+    public List<List<string>> GroupAnagrams(string[] strs)
+    {
+        var dict = new Dictionary<string, List<string>>(strs.Length);
+
+        foreach(var word in strs)
+        {
+            var arr = word.ToCharArray();
+            Array.Sort(arr);
+            var key = new String(arr);
+
+            if (!dict.TryGetValue(key, out var group))
+            {
+                dict[key] = group = new List<string>();
+            }
+            group.Add(word);
+
+            // if (!dict.ContainsKey(key))
+            // {
+            //     dict[key] = new List<string> {word};
+            // }
+            // else
+            // {
+            //     dict[key].Add(word);
+            // }
+        } 
+
+        return new List<List<string>>(dict.Values);
+    }
+}
