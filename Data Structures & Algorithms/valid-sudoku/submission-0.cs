9e82eaaed18f2922e399efc01b320fb9f7025ae1
hgrigor94 <60913448+hgrigor94@users.noreply.github.com>
2026-08-07 13:02:09 +0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cs
@@ -0,0 +1,54 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board)
+    {
+        foreach (var row in board)
+        {
+            var seen = new bool[10];
+            foreach (var c in row)
+            {
+                if (c == '.') continue;
+                
+                var i = c - '0';
+                if (seen[i]) return false;
+
+                seen[i] = true;
+            }
+        }
+
+        for (int c = 0; c < 9; c++)
+        {
+            var seen = new bool[10];
+            for (int r = 0; r < 9; r++)
+            {
+                if (board[r][c] == '.') continue;
+
+                var i = board[r][c] - '0';
+                if (seen[i]) return false;
+                
+                seen[i] = true;
+            }
+        }
+
+        for (int r = 0; r < 3; r++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                var seen = new bool[10];
+                for (int row = r*3; row < r*3+3; row++)
+                {
+                    for (int col = c*3; col < c*3+3; col++)
+                    {
+                        if (board[row][col] == '.') continue;
+
+                        var i = board[row][col] - '0';
+                        if (seen[i]) return false;
+
+                        seen[i] = true;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
