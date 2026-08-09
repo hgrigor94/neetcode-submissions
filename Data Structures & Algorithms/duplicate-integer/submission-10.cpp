33a1c4a677d6466a63b1bccb595d3f29915c8c65
hgrigor94 <60913448+hgrigor94@users.noreply.github.com>
2026-08-09 23:17:29 +0400

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums)
+    {
+        std::unordered_set<int> mySet;
+        mySet.reserve(nums.size());
+
+        for (int num: nums)
+        {
+            if(!mySet.insert(num).second)
+                return true;
+        }
+        return false;
+    }
+};
