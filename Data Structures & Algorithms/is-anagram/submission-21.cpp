8123cfd58a6c1140c498d2432d34cba57ef362d5
hgrigor94 <60913448+hgrigor94@users.noreply.github.com>
2026-08-09 23:59:55 +0400

    Add: is-anagram - submission-21

---
+++ Data Structures & Algorithms/is-anagram/submission-21.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.size() != t.size())
+            return false;
+
+        std::array<int, 26> letter_count{};
+
+        for (char c : s)
+        {
+            ++letter_count[c - 'a'];            
+        }
+
+        for (char c : t)
+        {
+            if(--letter_count[c - 'a'] < 0)
+                return false;            
+        }
+
+        return true;
+
+    }
+};
