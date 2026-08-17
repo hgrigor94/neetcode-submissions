d038b5a3538be8188de0868289b34aa6fa9a25d0
hgrigor94 <60913448+hgrigor94@users.noreply.github.com>
2026-08-18 00:42:36 +0400

    Add: remove-node-from-end-of-linked-list - submission-6

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-6.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        std::vector<ListNode*> nodes;
+
+        ListNode* current = head;
+        while (current != nullptr) {
+            nodes.push_back(current);
+            current = current->next;
+        }
+
+        int sz = static_cast<int>(nodes.size());
+        if(n == sz) {
+            return head->next;
+        }
+        if(n == 1) {
+            (nodes[sz - 2])->next = nullptr;
+            return head;
+        }
+
+        nodes[sz - n - 1]->next = nodes[sz - n + 1];
+        return head;
+    }
+};
