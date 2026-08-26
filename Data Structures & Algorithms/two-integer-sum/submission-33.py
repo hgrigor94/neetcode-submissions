4f830ab3548005ad4532607c4d44e43dd6577745
hgrigor94 <60913448+hgrigor94@users.noreply.github.com>
2026-08-26 11:43:04 +0400

    Add: two-integer-sum - submission-33

---
+++ Data Structures & Algorithms/two-integer-sum/submission-33.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {el: i for (i, el) in enumerate(nums)}
+        L = len(nums)
+        get = d.get
+        return next([i, j] for i in range(L) if (j := get(target - nums[i], False)) and i != j)
+        
